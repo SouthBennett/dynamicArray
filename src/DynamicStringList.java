e7972529fb98a6b898991529031051f2f6cb742d
Connor <connorjohnhughes@gmail.com>
2026-01-08 14:36:13 -0800

    Added Methods to DynamicStringList

--- src/DynamicStringList.java
+++ src/DynamicStringList.java
@@ -1,3 +1,39 @@
-public class MyStringArray {
+public class DynamicStringList implements StringList{
+
+    @Override
+    public String get(int index) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'get'");
+    }
+
+    @Override
+    public void set(int index, String value) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'set'");
+    }
+
+    @Override
+    public void add(String value) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'add'");
+    }
+
+    @Override
+    public String remove(int index) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'remove'");
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'size'");
+    }
+
+    @Override
+    public int capacity() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'capacity'");
+    }
     
 }
